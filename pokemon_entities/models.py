6a76de1b14d5f907f81ff97b0e48d319098c2699
Роман <frideriko26_93@mail.ru>
2024-10-18 23:22:38 +0300

    show pokemons on mainpage, show pokemons on mainpage map.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,6 +1,7 @@
 from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     image = models.ImageField(null=True, blank=True)
 
